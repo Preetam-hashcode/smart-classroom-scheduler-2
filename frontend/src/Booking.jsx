e0d31e63560966efd7947598cd99387a9ac7e2c0
Preetam Mondal <mondalpreetam9@gmail.com>
2025-09-10 23:28:43 +0530

    update time slot

--- frontend/src/Booking.jsx
+++ frontend/src/Booking.jsx
@@ -220,6 +220,7 @@ export default function Booking() {
   const timeSlots = [
     "all",
     "7-8",
+    "8-9",
     "9-10",
     "10-11",
     "11-12",
@@ -229,7 +230,7 @@ export default function Booking() {
     "15-16",
     "16-17",
     "17-18",
-    "19-20",
+    "18-19"
   ];
 
   function parseEndTimeToHour(endTime) {
